48b013233d4ed85d7aceeb11f63cfcbdf86e60d7
HeXis-YS <leonador@yeah.net>
2022-11-30 15:03:51 +0800

    Update to v107.1.0

--- build.sh
+++ build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-#workdir=${HOME}/build
+# workdir=${HOME}/build
 workdir=$(pwd)/build
 srclib=${workdir}/srclib
 ndk=r21d
 
-Fenix_tag=v106.1.0
-Fenix_version=106.1.0
-Fenix_code=1061020
+Fenix_tag=v107.1.0
+Fenix_version=107.1.0
+Fenix_code=171020
 
-MozBuild_commit=e16e430392a6561bcdc35d345710c7d7f911cef8
-MozAndroidComponents_tag=v106.0.5
+FirefoxAndroid_tag=v107.0.7
+# MozAndroidComponents_tag=v106.0.5
 MozAndroidComponentsAS_tag=v104.0.2
-MozAppServices_tag=v94.2.1
-MozFennec_tag=FIREFOX_106_0_RELEASE
-MozGlean_tag=v51.2.0
-MozGleanAS_tag=v50.1.3
+MozAppServices_tag=v94.2.2
+MozBuild_commit=4c988523b5c24f399150f84d0f681fbbd6502999
+MozFennec_tag=FIREFOX_107_0_RELEASE
+MozGlean_tag=v51.4.0
+MozGleanAS_tag=51.2.0
 rustup_tag=1.25.1
 wasisdk_tag=wasi-sdk-16
 
@@ -49,30 +50,44 @@ sdkmanager "ndk;${ndk}"
 sdkmanager "ndk;21.3.6528147"
 sdkmanager "platform-tools"
 
+# FireFoxAndroid
+git clone https://github.com/mozilla-mobile/firefox-android.git ${srclib}/FireFoxAndroid
+pushd ${srclib}/FireFoxAndroid
+sed -i -e '/com.google.firebase/d' android-components/buildSrc/src/main/java/Dependencies.kt
+rm -fR android-components/components/lib/push-firebase
+popd
+
 # MozBuild
 git clone https://github.com/HeXis-YS/fenixbuild.git ${srclib}/MozBuild
-cd ${srclib}/MozBuild
+pushd ${srclib}/MozBuild
 git checkout ${MozBuild_commit}
+popd
 
 # MozAndroidComponents
-git clone --depth=1 --branch ${MozAndroidComponents_tag} https://github.com/mozilla-mobile/android-components.git ${srclib}/MozAndroidComponents
-cd ${srclib}/MozAndroidComponents
-sed -i -e '/com.google.android.gms/d; /com.google.firebase/d' buildSrc/src/main/java/Dependencies.kt && rm -fR components/feature/p2p components/lib/{nearby,push-amazon,push-firebase}
+# git clone --depth=1 --branch ${MozAndroidComponents_tag} https://github.com/mozilla-mobile/android-components.git ${srclib}/MozAndroidComponents
+# pushd ${srclib}/MozAndroidComponents
+# sed -i -e '/com.google.android.gms/d; /com.google.firebase/d' buildSrc/src/main/java/Dependencies.kt
+# rm -fR components/feature/p2p components/lib/{nearby,push-amazon,push-firebase}
+# popd
 
 # MozAndroidComponentsAS
 git clone --depth=1 --branch ${MozAndroidComponentsAS_tag} https://github.com/mozilla-mobile/android-components.git ${srclib}/MozAndroidComponentsAS
-cd ${srclib}/MozAndroidComponentsAS
-sed -i -e '/com.google.android.gms/d; /com.google.firebase/d' buildSrc/src/main/java/Dependencies.kt && rm -fR components/feature/p2p components/lib/{nearby,push-amazon,push-firebase}
+pushd ${srclib}/MozAndroidComponentsAS
+sed -i -e '/com.google.android.gms/d; /com.google.firebase/d' buildSrc/src/main/java/Dependencies.kt
+rm -fR components/feature/p2p components/lib/{nearby,push-amazon,push-firebase}
+popd
 
 # MozAppServices
 git clone --depth=1 --branch ${MozAppServices_tag} https://github.com/mozilla/application-services.git ${srclib}/MozAppServices
-cd ${srclib}/MozAppServices
+pushd ${srclib}/MozAppServices
 git submodule update --init --depth=1
+popd
 
 # MozFennec
 hg clone --stream https://hg.mozilla.org/releases/mozilla-release/ ${srclib}/MozFennec
-cd ${srclib}/MozFennec
+pushd ${srclib}/MozFennec
 hg checkout ${MozFennec_tag}
+popd
 
 # MozGlean
 git clone --depth=1 --branch ${MozGlean_tag} https://github.com/mozilla/glean.git ${srclib}/MozGlean
@@ -86,8 +101,9 @@ git clone --depth=1 --branch ${rustup_tag} https://github.com/rust-lang/rustup $
 # wasi-sdk
 git clone --depth=1 --branch ${wasisdk_tag} https://github.com/WebAssembly/wasi-sdk.git ${srclib}/wasi-sdk
 git clone https://git.savannah.gnu.org/git/config.git ${srclib}/wasi-sdk/src/config
-cd ${srclib}/wasi-sdk
+pushd ${srclib}/wasi-sdk
 git submodule update --init --depth=1
+popd
 
 # Fenix
 git clone --depth=1 --branch ${Fenix_tag} https://github.com/mozilla-mobile/fenix ${workdir}/fenix
